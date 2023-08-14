e5acd6fd04ea8c7ab4d606071e7fc54c8613bde0
AV <87594856+vakhil03@users.noreply.github.com>
2023-08-14 01:16:02 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,47 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills,
+        // skills: skillsMember,
+        // skills: function() {
+        //     return skills;
+        // },
+        // skills: () => {
+        //     return skills;
+        // },
+        // skills: () => skills,
+        // skills: function() {
+        //     return this.skills;
+        // },
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+        // skills: () => this.skills,
+        // skills: () => {
+        //     return this.skills;
+        // },
+    };
+    return member;
+}
